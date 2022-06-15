364208d7de81fd680b9c5a99b72fb8ba5559613f
ftolentino <filmer.tolentino@gmail.com>
2022-06-15 16:01:33 -0700

    WIP Business logic
    <Co-authored by Joe Roaden <josephroaden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-// Business Logic for Dice
-function randomizeNumber (min, max) {
-  min = Math.ceil(min);
+// Business Logic for Player Scores
+function ScoreTable () {
+  this.score = {};
+}
+PlayerOneScore.prototype.addScore = function(score){
+  
+}
+// Business Logic for Players
+function Players(playerOneScore, playerTwoScore) {
+  this.playerOneScore = playerOneScore;
+  this.playerTwoScore = playerTwoScore;
+}
 
-  max = Math.floor(max);
+Players.prototype.scoreSum = function () {
+  let diceRollNumber = diceRoll(1, 6);
+  return this.playerOneScore = diceRollNumber;
+}
+
+// Business Logic for Dice
+function diceRoll (min, max) {
+   min = Math.ceil(min);
+
+   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -19,7 +37,7 @@ function randomizeNumber (min, max) {
 $(document).ready(function() {
   $("button#dice-button").click(function(event) {
     event.preventDefault();
-    let diceRollNumber = randomizeNumber(1, 6);
+    let diceRollNumber = diceRoll(1, 6);
     console.log(diceRollNumber);
     
 
