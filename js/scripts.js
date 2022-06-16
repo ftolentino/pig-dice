03008e1a93068cd660d5b50e9a7af9061c7dab02
ftolentino <filmer.tolentino@gmail.com>
2022-06-16 11:40:14 -0700

    Add playerTwo HTML
    <Co-authored by Joe Roaden <josephroaden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 // Business Logic for Player Scores
 function ScoreTable () {
-  // this.diceNumber = function () {
-  //   Math.floor(Math.random() * 6) + 1;
-  // }
-  this.score = 0;
   this.diceNumber = 0;
   this.diceSum = 0;
 }
 
 let playerOne = new ScoreTable();
+let playerTwo = new ScoreTable();
 
 
 // Business Logic for Dice
@@ -29,14 +26,16 @@ ScoreTable.prototype.scoreSum = function () {
 // UI Logic
 
 $(document).ready(function() {
-  $("button#dice-button").click(function(event) {
+  $("#dice-button").click(function(event) {
     event.preventDefault();
+    
     playerOne.diceRoll();
     playerOne.scoreSum();
 
     console.log(playerOne);
    
 
-   $('ul#dice-number').show().html(playerOne);
+   $('#dice-number').show().html(playerOne.diceNumber);
+   $('#dice-sum').show().html(playerOne.diceSum);
   });
 });
